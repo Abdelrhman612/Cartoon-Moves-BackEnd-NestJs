95cd430b36d1cb645817cf992f5d85a7ae2eb5b3
Abdelrhman <abdo612200320@gmail.com>
2025-06-04 21:31:07 +0300

    update Auth.Guards.ts

--- src/utils/Auth.Guards.ts
+++ src/utils/Auth.Guards.ts
@@ -1,51 +1,60 @@
 import {
   CanActivate,
   ExecutionContext,
-  ForbiddenException,
+  Injectable,
   UnauthorizedException,
+  ForbiddenException,
 } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
+import { Reflector } from '@nestjs/core';
 import { Roles } from './decorator.role';
-
-export class AuthGuards implements CanActivate {
+@Injectable()
+export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private reflector: Reflector,
   ) {}
-  async canActivate(context: ExecutionContext): Promise<boolean> {
+
+  async canActivate(context: ExecutionContext) {
     const request = context
       .switchToHttp()
       .getRequest<{ headers: { authorization?: string } }>();
     const authHeader = request.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1];
+    const token = authHeader?.split(' ')[1];
+
     if (!token) {
       throw new UnauthorizedException('Missing authentication token');
     }
+
     try {
       const tokenPayload = await this.jwtService.verifyAsync<{
+        id: string;
         email: string;
         role: string;
-        id: string;
       }>(token, {
         secret: process.env.JWT_SECRET as string,
       });
+
       request['user'] = tokenPayload;
-      const requreRoles = this.reflector.get<string[]>(
-        Roles,
-        context.getHandler(),
-      );
-      if (!requreRoles) {
+
+      const requiredRoles = this.reflector.get(Roles, context.getHandler());
+
+      if (!requiredRoles) {
         return true;
       }
-      if (!requreRoles.includes(tokenPayload.role)) {
+
+      if (!requiredRoles.includes(tokenPayload.role)) {
         throw new ForbiddenException(
-          `You need one of these roles: ${requreRoles.join(', ')}`,
+          `You need one of these roles: ${requiredRoles.join(', ')}`,
         );
       }
+
       return true;
-    } catch {
-      throw new UnauthorizedException('Invalid authentication token');
+    } catch (error) {
+      if (error instanceof ForbiddenException) {
+        throw error;
+      }
+      throw new UnauthorizedException('Invalid or expired token');
     }
   }
 }
