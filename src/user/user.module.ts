b409138086164949093b30ea55111aca49b276fe
Abdelrhman <abdo612200320@gmail.com>
2025-06-04 21:30:55 +0300

    update user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET as string,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
