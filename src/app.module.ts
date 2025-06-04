a9bf28a5476d1b6d3b3cfb853cc53ffa2180988f
Abdelrhman <abdo612200320@gmail.com>
2025-06-04 11:57:25 +0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './DataBase/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [PrismaModule, UserModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
