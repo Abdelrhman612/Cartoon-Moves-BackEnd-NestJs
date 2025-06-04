1646d2f58a4f282819dd0463e05b11e056870d3d
Abdelrhman <abdo612200320@gmail.com>
2025-06-04 12:48:55 +0300

    update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,17 +6,20 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create.user.dto';
 import { UpdateUserDto } from './dto/update.user.dto';
+import { AuthGuards } from 'src/utils/Auth.Guards';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @UseGuards(AuthGuards)
   getUsers() {
     return this.userService.GetUsers();
   }
