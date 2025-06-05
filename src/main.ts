2b73feff9581a5ed1f18d61bccf54fb4a9d80ed1
Abdelrhman <abdo612200320@gmail.com>
2025-06-06 02:44:56 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
     credentials: true,
   });
   app.setGlobalPrefix('api/v2');
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT as string);
 }
 void bootstrap();
