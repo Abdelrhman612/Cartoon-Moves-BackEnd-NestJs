8f1b9fe58781806f0286cf6dfd372dd0ec6238b4
Abdelrhman <abdo612200320@gmail.com>
2025-06-12 14:48:37 +0300

    feat: create create-movie.dto.ts

---
+++ src/movie/dto/create-movie.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsUrl } from 'class-validator';
+
+export class CreateMovieDto {
+  @IsString({ message: 'title must be a string' })
+  title: string;
+  @IsString({ message: 'description must be a string' })
+  description: string;
+  @IsUrl({}, { message: 'must be a url' })
+  posterUrl;
+  @IsUrl({}, { message: 'image must be a url' })
+  image: string;
+}
