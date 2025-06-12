a3d6546ccacfef931193223f67171ea124b0ca82
Abdelrhman <abdo612200320@gmail.com>
2025-06-12 14:50:11 +0300

    update movie.module.ts

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { MovieController } from './movie.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET as string,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+    }),
+  ],
   controllers: [MovieController],
   providers: [MovieService],
 })
