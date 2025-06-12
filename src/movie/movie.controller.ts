c84abed3ec4bcdd36ca0257cb758da4f91a7614b
Abdelrhman <abdo612200320@gmail.com>
2025-06-12 14:48:20 +0300

    feat: create movie.controller.ts

---
+++ src/movie/movie.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { MovieService } from './movie.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movie')
+export class MovieController {
+  constructor(private readonly movieService: MovieService) {}
+
+  @Post()
+  async create(@Body() createMovieDto: CreateMovieDto) {
+    return this.movieService.createMovie(createMovieDto);
+  }
+  @Get()
+  findAll() {
+    return this.movieService.GetMovies();
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
+    return this.movieService.updateMovie(id, updateMovieDto);
+  }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.movieService.DeleteMovie(id);
+  }
+}
