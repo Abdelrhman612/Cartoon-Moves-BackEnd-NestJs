3e2ef467e4b554e5a3921eab423ccbf505e4b44d
Abdelrhman <abdo612200320@gmail.com>
2025-06-14 13:55:16 +0300

    update movie.controller.ts

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -6,16 +6,21 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Roles } from 'src/utils/decorator.role';
+import { AuthGuard } from 'src/utils/Auth.Guards';
 
 @Controller('movie')
+@UseGuards(AuthGuard)
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Post()
+  @Roles(['admin'])
   async create(@Body() createMovieDto: CreateMovieDto) {
     return this.movieService.createMovie(createMovieDto);
   }
@@ -24,10 +29,12 @@ export class MovieController {
     return this.movieService.GetMovies();
   }
   @Patch(':id')
+  @Roles(['admin'])
   update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
     return this.movieService.updateMovie(id, updateMovieDto);
   }
   @Delete(':id')
+  @Roles(['admin'])
   remove(@Param('id') id: string) {
     return this.movieService.DeleteMovie(id);
   }
