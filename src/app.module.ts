3c38f1fcc7cae626f91209654af9f0ab8e24d51b
Abdelrhman <abdo612200320@gmail.com>
2025-06-21 00:08:17 +0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,18 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './DataBase/prisma.module';
 import { AuthModule } from './Auth/Auth.Module';
 import { MovieModule } from './movie/movie.module';
+import { ReveiewModule } from './Review/reveiw.module';
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, ConfigModule.forRoot(), MovieModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    ReveiewModule,
+    FavoriteModule,
+    ConfigModule.forRoot(),
+    MovieModule,
+  ],
 })
 export class AppModule {}
