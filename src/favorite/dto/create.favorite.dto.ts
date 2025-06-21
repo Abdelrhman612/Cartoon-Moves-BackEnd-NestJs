89afcb195f12f457515ec3676074a1acb0ec1896
Abdelrhman <abdo612200320@gmail.com>
2025-06-21 14:36:24 +0300

    update create.favorite.dto.ts

--- src/favorite/dto/create.favorite.dto.ts
+++ src/favorite/dto/create.favorite.dto.ts
@@ -3,7 +3,4 @@ import { IsString } from 'class-validator';
 export class CreateFavoriteDto {
   @IsString({ message: 'Movie ID must be a string' })
   movieId: string;
-
-  @IsString({ message: 'User ID must be a string' })
-  userId: string;
 }
