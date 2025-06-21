c4724c07055e3fff38fd849f72d4775da61b4c1d
Abdelrhman <abdo612200320@gmail.com>
2025-06-21 14:36:12 +0300

    update favorite.controller.ts

--- src/favorite/favorite.controller.ts
+++ src/favorite/favorite.controller.ts
@@ -6,20 +6,31 @@ import {
   Param,
   Get,
   ValidationPipe,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
 import { CreateFavoriteDto } from './dto/create.favorite.dto';
+
+import { Request } from 'express';
+import { AuthGuard } from 'src/utils/Auth.Guards';
 
 @Controller('favorites')
 export class FavoriteController {
   constructor(private readonly favoriteService: FavoriteService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   add(
     @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
     dto: CreateFavoriteDto,
+    @Req() req: Request,
   ) {
-    return this.favoriteService.addFavorite(dto);
+    const userId = (req['user'] as { id: string } | undefined)?.id;
+    if (!userId) {
+      throw new Error('User ID not found in request');
+    }
+    return this.favoriteService.addFavorite(dto, userId);
   }
 
   @Get(':userId')
